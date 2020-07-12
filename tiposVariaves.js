364ad404303ae65a5fb6ac5b38dd61f6e9ecc363
Eduardo da Silva Rodrigues <eduardodsr@gmail.com>
2020-07-12 02:31:14 -0300

    Primeiro Commit

---
+++ tiposVariaves.js
@@ -0,0 +1,14 @@
+var inteiro = 10;
+var decimal = 10.54;
+var string = "Texto de teste";
+var boolean = true;
+
+console.log (inteiro);
+console.log (decimal);
+console.log (string);
+console.log(boolean)
+
+console.log(typeof(inteiro));  // "typeof" retorna o tipo de variável
+console.log(typeof(decimal));
+console.log(typeof(string));
+console.log(typeof(boolean));
