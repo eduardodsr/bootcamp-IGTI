9744665dc05340a83ab97522710057b60a6bc468
Eduardo da Silva Rodrigues <66234125+eduardodsr@users.noreply.github.com>
2020-08-22 08:38:43 -0300

    Delete variaveis.js

--- variaveis.js
+++
@@ -1,8 +0,0 @@
-var a = 2;
-var b = 3;
-var total = a + b;
-
-a = 5;          // variavel 'a' já declarada, não precisa declarar novamente a mesama variavel.
-total = a + b;  // variavel 'total' já declarada, não precisa declarar novamente a mesama variavel.
-
-console.log(total);
