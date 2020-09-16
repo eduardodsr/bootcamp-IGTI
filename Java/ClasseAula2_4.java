8f696c370389323a0ecfc603462c31a3c57982a0
Eduardo da Silva Rodrigues <66234125+eduardodsr@users.noreply.github.com>
2020-09-16 12:08:56 -0300

    Add files via upload

---
+++ Java/ClasseAula2_4.java
@@ -0,0 +1,30 @@
+package PacoteAula2_4;
+
+public class ClasseAula2_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int nota = 20;
+		char tipoAluno = 'B';
+		double horasCursadas = 2.5;
+		boolean aprovado = true;
+		
+		System.out.println("Nota: " + nota);
+		System.out.println("Tipo de Aluno: " + tipoAluno);
+		System.out.println("Horas Cursadas: " + horasCursadas);
+		System.out.println("Aprovado: " + aprovado);
+		
+		nota = 30;
+		tipoAluno = 'G';
+		horasCursadas = 4.0;
+		aprovado = false;
+		
+		System.out.println("\nNota: " + nota);
+		System.out.println("Tipo de Aluno: " + tipoAluno);
+		System.out.println("Horas Cursadas: " + horasCursadas);
+		System.out.println("Aprovado: " + aprovado);
+		
+	}
+
+}
