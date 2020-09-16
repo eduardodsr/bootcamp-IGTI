dd745d6741e6270130cd540ff509bb1b3f54301b
Eduardo da Silva Rodrigues <66234125+eduardodsr@users.noreply.github.com>
2020-09-16 12:39:13 -0300

    Add files via upload
    11 - Aula 2.5 - O tipo String.mp4

---
+++ Java/ManipulandoString.java
@@ -0,0 +1,13 @@
+package pacote;
+
+public class ManipulandoString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String texto = "Texto entre aspas é uma String";
+		
+		System.out.println(texto);
+	}
+
+}
