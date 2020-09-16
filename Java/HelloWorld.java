651383beae6abe247cab82d6abbd58634a97ec03
Eduardo da Silva Rodrigues <66234125+eduardodsr@users.noreply.github.com>
2020-09-16 12:00:40 -0300

    Create HelloWorld.java

---
+++ Java/HelloWorld.java
@@ -0,0 +1,11 @@
+# HelloWorld.java
+
+public class Hello World {
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
+      int z=x+y;
+
+      System.out.println("Hello World");
+    }
+}
