02d3ebb2b53d662bb0b0cfdc6581918296f95195
Eduardo da Silva Rodrigues <66234125+eduardodsr@users.noreply.github.com>
2020-09-16 12:01:31 -0300

    Update HelloWorld.java

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -2,10 +2,6 @@
 
 public class Hello World {
     public static void main(String args[]) {
-      int x=10;
-      int y=25;
-      int z=x+y;
-
-      System.out.println("Hello World");
+           System.out.println("Hello World");
     }
 }
