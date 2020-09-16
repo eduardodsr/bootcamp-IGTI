22042fb8dcb9fb54e99209d3419d35b77c232ba9
Eduardo da Silva Rodrigues <66234125+eduardodsr@users.noreply.github.com>
2020-09-16 12:46:13 -0300

    Add files via upload

--- Java/ManipulandoString.java
+++ Java/ManipulandoString.java
@@ -5,9 +5,14 @@ public class ManipulandoString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String texto = "Texto entre aspas é uma String";
+		String texto = "Texto entre aspas é uma String.";
 		
 		System.out.println(texto);
+		
+		String nome = "Eduardo";
+		String frase = "Estou aprendendo a programar em Java";
+		
+		System.out.println("\nNome: " + nome + "\nFrase: " + frase);
 	}
 
 }
