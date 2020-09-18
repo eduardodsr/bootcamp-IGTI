cb83cf9c82273d72e3374a5be05b095effdacda9
Eduardo da Silva Rodrigues <66234125+eduardodsr@users.noreply.github.com>
2020-09-18 17:47:37 -0300

    Add files via upload

---
+++ Java/ProjetoAula2_8Operadores/ClasseSubtracao.java
@@ -0,0 +1,16 @@
+package PacoteOperadores;
+
+public class ClasseSubtracao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// subtração 			
+		int n1 = 0, n2 = 0, total = 0;
+		n1 = 10;
+		n2 = 20;
+		total = n2 - n1;		
+		System.out.printf("O resultado da subtração de %d - %d = %d.", n2, n1, total);
+	}
+
+}
