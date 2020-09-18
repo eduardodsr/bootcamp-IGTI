9b9cbc97ab66f76be981d1fbb332483c0e2ec62f
Eduardo da Silva Rodrigues <66234125+eduardodsr@users.noreply.github.com>
2020-09-18 18:03:45 -0300

    Update ClasseMultiplicacao.java

--- Java/ProjetoAula2_8Operadores/ClasseMultiplicacao.java
+++ Java/ProjetoAula2_8Operadores/ClasseMultiplicacao.java
@@ -5,12 +5,12 @@ public class ClasseMultiplicacao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// multiplicação			
-				int n1 = 0, n2 = 0, total = 0;
-				n1 = 2;
-				n2 = 3;
-				total = n1 * n2;		
-				System.out.printf("O resultado da multiplicação de %d * %d = %d.", n1, n2, total);
+		// multiplicaÃ§Ã£o			
+		int n1 = 0, n2 = 0, total = 0;
+		n1 = 2;
+		n2 = 3;
+		total = n1 * n2;		
+		System.out.printf("O resultado da multiplicaÃ§Ã£o de %d * %d = %d.", n1, n2, total);
 	}
 
 }
