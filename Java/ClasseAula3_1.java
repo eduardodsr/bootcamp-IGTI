6293590ed18712ef4435baa1a67142ab772e4955
Eduardo da Silva Rodrigues <66234125+eduardodsr@users.noreply.github.com>
2020-09-18 22:19:55 -0300

    Add files via upload

--- Java/ClasseAula3_1.java
+++ Java/ClasseAula3_1.java
@@ -12,11 +12,11 @@ public class ClasseAula3_1 {
 				
 		Scanner entradaCaracteres = new Scanner(System.in);
 		
-		System.out.print("Digite o nome do aluno: ");
-		String nomeAluno = entradaCaracteres.nextLine();
+		System.out.print("Digite o nome do aluno: "); // entrada de dados
+		String nomeAluno = entradaCaracteres.nextLine(); // nomeAluno
 		
-		System.out.print("Digite o sexo do aluno (M ou F): ");
-		sexo = entradaCaracteres.nextLine().charAt(0);
+		System.out.print("Digite o sexo do aluno (M ou F): "); // entrada de dados
+		sexo = entradaCaracteres.nextLine().charAt(0);	// sexo (M ou F)
 		
 		System.out.printf("\nNome: %s \nSexo: %c", nomeAluno, sexo);
 			
