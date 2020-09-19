5075b1ac9c069669c25b129ad8c88b049b7811ba
Eduardo da Silva Rodrigues <66234125+eduardodsr@users.noreply.github.com>
2020-09-18 21:51:08 -0300

    Update and rename Classe2_13.java to ClasseAula2_13.java

--- Java/ClasseAula2_13.java
+++ Java/ClasseAula2_13.java
@@ -2,7 +2,7 @@ package PacoteExemplo;
 
 import java.math.*; 
 
-public class Classe2_13 {
+public class ClasseAula2_13 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,20 +12,18 @@ public class Classe2_13 {
 		boolean aprovado;
 		String nomeAluno;
 		
-		nomeAluno = "Joaquim José da Silva Xavier";
+		nomeAluno = "Joaquim JosÃ© da Silva Xavier";
 		sexo = 'M';
 		
 		nota = 75.50;
 		
-		frequencia = 0.60; // é a mesma coisa que 60%
+		frequencia = 0.60; // Ã© a mesma coisa que 60%
 		
 		aprovado = ( (nota >= 70) && (frequencia >= 0.75) );
 		
-		System.out.printf("O aluno %s, sexo %c, obteve nota: %.2f e frequência: %.2f %%.  \n"
+		System.out.printf("O aluno %s, sexo %c, obteve nota: %.2f e frequÃªncia: %.2f %%.  \n"
 				+ "Aluno Aprovado? %b", nomeAluno, sexo, nota, frequencia*100,  aprovado);	
-			
-		
-			
+							
 
 	}
 
